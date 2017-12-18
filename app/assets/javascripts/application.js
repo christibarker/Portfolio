b5b47cd9992bba2c7db84f4cc7629e2849221f81
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-18 09:57:32 -0700

    gem setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require materialize-sprockets
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
